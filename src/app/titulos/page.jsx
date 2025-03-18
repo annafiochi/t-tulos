1b60a00600d05dbbd618357fa1621b8d34b56461
annafiochi <158165846+annafiochi@users.noreply.github.com>
2025-03-18 11:33:41 -0300

    feat: creat section component

--- src/app/titulos/page.jsx
+++ src/app/titulos/page.jsx
@@ -3,6 +3,7 @@ import styles from "./titulos.module.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import Card from "../components/cards";
+import Section from "../components/section";
 
 
 export default function Titulo() {
@@ -10,9 +11,9 @@ export default function Titulo() {
     <div className={styles.container}>
       <Header/>
       <main className={styles.main}>
-        <section className={styles.featured}>
-          <h2>Conquistas Principais</h2>
-          <div className={styles.featuredTrophies}>
+
+        <Section titulo={"Conquistas principais"}>
+
            <Card ano ={2012} capa={"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"} 
            detalhes={"Corinthians 1 x 0 Chelsea"} 
            textAlt={"Troféu do mundial interclubes 2012"} 
@@ -30,70 +31,12 @@ export default function Titulo() {
            textAlt={"Trofeu copa libertadores da america 2000"}
            titulo={"Mundial de clubes da FIFA"} 
            />
-            <div className={styles.trophy}>
-              <div className={styles.trophyCover}>
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"
-                  alt="Troféu da Copa Libertadores da América 2000"
-                />
-              </div>
-              <div className={styles.trophyInfo}>
-                <h3>Mundial de Clubes da FIFA</h3>
-                <p className={styles.year}>2000</p>
-                <p className={styles.details}>Corinthians 0(4) x 0(3) Vasco</p>
-                <button className={styles.detailsButton}>Ver Detalhes</button>
-              </div>
-            </div>
-          </div>
-        </section>
+        </Section>
 
-        <section className={styles.newTrophies}>
-          <h2>Campeonatos Brasileiros</h2>
-          <div className={styles.trophiesGrid}>
-            <div className={styles.trophy}>
-              <div className={styles.trophyCover}>
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"
-                  alt="Troféu do Campeonato Brasileiro 2017"
-                />
-              </div>
-              <div className={styles.trophyInfo}>
-                <h3>Campeonato Brasileiro</h3>
-                <p className={styles.year}>2017</p>
-                <p className={styles.details}>7º título brasileiro</p>
-                <button className={styles.detailsButton}>Ver Detalhes</button>
-              </div>
-            </div>
-            <div className={styles.trophy}>
-              <div className={styles.trophyCover}>
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"
-                  alt="Troféu do Campeonato Brasileiro 2015"
-                />
-              </div>
-              <div className={styles.trophyInfo}>
-                <h3>Campeonato Brasileiro</h3>
-                <p className={styles.year}>2015</p>
-                <p className={styles.details}>6º título brasileiro</p>
-                <button className={styles.detailsButton}>Ver Detalhes</button>
-              </div>
-            </div>
-            <div className={styles.trophy}>
-              <div className={styles.trophyCover}>
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"
-                  alt="Troféu do Campeonato Brasileiro 2011"
-                />
-              </div>
-              <div className={styles.trophyInfo}>
-                <h3>Campeonato Brasileiro</h3>
-                <p className={styles.year}>2011</p>
-                <p className={styles.details}>5º título brasileiro</p>
-                <button className={styles.detailsButton}>Ver Detalhes</button>
-              </div>
-            </div>
-          </div>
-        </section>
+        <Section titulo={"Campeonato brasileiro"}>
+
+        </Section>
+        
 
         <section className={styles.promo}>
           <h2>Campeonatos Estaduais</h2>
@@ -206,37 +149,7 @@ export default function Titulo() {
         </section>
       </main>
 
-      <footer className={styles.footer}>
-        <div className={styles.footerContent}>
-          <div className={styles.footerSection}>
-            <h3>Sobre o Sport Club Corinthians Paulista</h3>
-            <p>
-              Fundado em 1910, o Corinthians é um dos clubes mais populares e
-              vitoriosos do futebol brasileiro, com milhões de torcedores ao
-              redor do mundo.
-            </p>
-          </div>
-          <div className={styles.footerSection}>
-            <h3>Contato</h3>
-            <p>Email: contato@corinthians.com.br</p>
-            <p>Telefone: (11) 2095-3000</p>
-          </div>
-          <div className={styles.footerSection}>
-            <h3>Siga-nos</h3>
-            <div className={styles.socialLinks}>
-              <a href="#">Facebook</a>
-              <a href="#">Instagram</a>
-              <a href="#">Twitter</a>
-            </div>
-          </div>
-        </div>
-        <div className={styles.copyright}>
-          <p>
-            &copy; 2025 Sport Club Corinthians Paulista. Todos os direitos
-            reservados.
-          </p>
-        </div>
-      </footer>
+      <Footer/>
     </div>
   );
 };
